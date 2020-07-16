44e9a424eb70d9d4342fb3963865a428d970b07f
Aditya <adityagodofgamer.1@gmail.com>
2020-07-16 18:41:16 +0530

    Converted to AJAX

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -31,6 +31,16 @@ module.exports.destroy=async function(req,res){
             await Comment.deleteMany({
                 post:req.params.id
             });
+
+            if(req.xhr){
+                return res.status(200).json({
+                    data:{
+                        post_id:req.params.id
+                    },
+                    message:'Post Deleted Successfully'
+                });
+            }
+
             req.flash('success','Post deleted');
             return res.redirect('back');
         }else{
@@ -38,7 +48,7 @@ module.exports.destroy=async function(req,res){
             return res.redirect('back');
         }
 
-    }catch{
+    }catch(err){
         req.flash('error',err);
         return;
     }
