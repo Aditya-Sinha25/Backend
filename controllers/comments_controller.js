8ee255bca48d5dda0c00dac6f6d3995c622d025d
Aditya <adityagodofgamer.1@gmail.com>
2020-07-20 13:45:02 +0530

    Mailer added

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,20 +1,36 @@
 const Comment =require('../models/comment');
 const Post =require('../models/post');
+const commentsMailer =require('../mailer/comments_mailer');
 
-module.exports.create=function(req,res){
-    Post.findById(req.body.post,function(err,post){
+module.exports.create =async function(req,res){
+    try{
+        let post =await Post.findById(req.body.post);
         if(post){
-            Comment.create({
+            let comment =await Comment.create({
                 content:req.body.content,
                 post:req.body.post,
                 user:req.user._id
-            },function(err,comment){
-                post.comments.push(comment);
-                post.save();
-                res.redirect('/');
             });
-        }        
-    });
+            post.comments.push(comment);
+            post.save();
+
+            comment =await comment.populate('user','name email').execPopulate();
+            commentsMailer.newComment(comment);
+            if(req.xhr){
+                return res.status(200).json({
+                    data:{
+                        comment:comment
+                    },
+                    message:'Post Created'
+                });
+            }
+            req.flash('success','Comment Published');
+            res.redirect('/');
+        }
+    }catch(err){
+        req.flash('error',err);
+        return;
+    }
 }
 
 module.exports.destroy=function(req,res){
